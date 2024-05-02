de91e1c533c03beb7a55c7e647573fdb0df3dbb3
Masud Parvez <masud.parvez@brainstation.com>
2024-05-02 18:07:12 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Advance_CSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
