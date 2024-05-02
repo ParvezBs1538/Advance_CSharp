6ba53e10116c9f8f6791a997015e7dc55407a734
Masud Parvez <masud.parvez@brainstation.com>
2024-05-02 18:12:19 +0600

    Object Initializer in C#

--- Program.cs
+++ Program.cs
@@ -1,10 +1,25 @@
 ﻿namespace Advance_CSharp
 {
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Student student = new Student()
+            {
+                Id = 38,
+                Name = "Parvez",
+                Age = 25,
+            }; 
+            Console.WriteLine(student.Id);
+            Console.WriteLine(student.Name);
+            Console.WriteLine(student.Age);
         }
     }
 }
