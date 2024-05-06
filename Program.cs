3f2d535997658fc895b164115a11d88ebdbe2eee
Masud Parvez <masud.parvez@brainstation.com>
2024-05-06 10:21:09 +0600

    Jagged Array in C#

--- Program.cs
+++ Program.cs
@@ -1,46 +1,20 @@
 ﻿namespace Advance_CSharp
 {
-    class Student
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int Age { get; set; }
-        public string Address { get; set; }
-    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Student student1 = new Student()
-            {
-                Id = 35,
-                Name = "Masud",
-                Age = 25,
-            };
-            Student student2 = new Student()
-            {
-                Id = 38,
-                Name = "Parvez",
-                Age = 26,
-            };
+            int[][] jarr = new int[2][];
+            jarr[0] = new int[3] { 1, 2, 3 };
+            jarr[1] = new int[5] { 1, 2, 3, 4, 5 };
 
-            List<Student> studentList = new List<Student>()
+            for (int i = 0; i < jarr.Length; i++)
             {
-                student1,
-                student2,
-                new Student() {Id = 15, Name = "Rifat", Age = 20},
-            };
-            Student student4 = new Student()
-            {
-                Id = 21,
-                Name = "Saif",
-                Age = 21,
-            };
-            studentList.Add(student4);
-
-            foreach (var i in studentList)
-            {
-                Console.WriteLine($"{i.Id} {i.Name} {i.Age}");
+                for (int j = 0; j < jarr[i].Length; j++)
+                {
+                    Console.Write(jarr[i][j] + " ");
+                }
+                Console.WriteLine();
             }
         }
     }
