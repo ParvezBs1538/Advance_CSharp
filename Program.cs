7e5795b281e8ddfc9ad216913d7c8622bad55049
Masud Parvez <masud.parvez@brainstation.com>
2024-05-06 12:34:02 +0600

    Generics class & methods

--- Program.cs
+++ Program.cs
@@ -1,21 +1,45 @@
-﻿namespace Advance_CSharp
+﻿using System;
+
+namespace Advance_CSharp
 {
+    // Generic Class
+    public class Comparison<T>
+    {
+        public bool isEqual(T val1, T val2)
+        {
+            return val1.Equals(val2);
+        }
+    }
+    // Generic Method
+    public class Comparison
+    {
+        public bool isEqual<T>(T val1, T val2)
+        {
+            return val1.Equals(val2);
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            int[][] jarr = new int[2][];
-            jarr[0] = new int[3] { 1, 2, 3 };
-            jarr[1] = new int[5] { 1, 2, 3, 4, 5 };
+            // Use Generic Class
+            Comparison<int> comp1 = new Comparison<int>();
+            bool isEqual1 = comp1.isEqual(2, 2);
 
-            for (int i = 0; i < jarr.Length; i++)
-            {
-                for (int j = 0; j < jarr[i].Length; j++)
-                {
-                    Console.Write(jarr[i][j] + " ");
-                }
-                Console.WriteLine();
-            }
+            Comparison<string> comp2 = new Comparison<string>();
+            bool ieEqual2 = comp2.isEqual("Name", "name");
+
+            Console.WriteLine(isEqual1 + " " + ieEqual2);
+
+
+            // Use Generic Method
+            Comparison comp3 = new Comparison();
+            bool isEqual3 = comp3.isEqual<int>(2, 3);
+
+            Comparison comp4 = new Comparison();
+            bool isEqual4 = comp4.isEqual<string>("ID", "ID");
+
+            Console.WriteLine(isEqual3 + " " + isEqual4);
         }
     }
 }
