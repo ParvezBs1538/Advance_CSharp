319dc254a494f791f405f14a3c9838f5b785b072
Masud Parvez <masud.parvez@brainstation.com>
2024-05-06 11:25:49 +0600

    Add & Sub using Generic function in Delegate

--- Program.cs
+++ Program.cs
@@ -3,29 +3,33 @@ namespace Advance_CSharp
 {
     internal class Program
     {
-        public delegate void Calculator(int num1, int num2);
+        public delegate void Calculator<T, U>(T num1, U num2); 
+        // <T,U> means its can contains two difference or same type parameter
         static void Main(string[] args)
         {
-            //Calculator calculate = Add;
-            Calculator calculate = new Calculator(Add);
-            //Calculator calculate = (x, y) => Add(x, y);
+            //Calculator<int, float> calculate = Add;
+            //Calculator<int, float> calculate = new Calculator<int, float>(Add<int, float>);
+            Calculator<int,float> calculate = (x, y) => Add(x, y);
 
-        // calculate sum
-            calculate.Invoke(40, 12);
-            //calculate(40, 12);
+            // calculate sum
+            //calculate.Invoke(40, 12.5f);
+            calculate = Add<int, float>;
+            calculate(40, 12.5f);
 
-        // calculate sub
-            calculate = Sub;
-            calculate(40, 12);
+            // calculate sub
+            calculate = Sub<int, float>;
+            calculate(40, 20.5f);
         }
-        public static void Add(int firstNumber, int secondNumber)
+        public static void Add<T, U>(T firstNumber, U secondNumber)
         {
-            int ans = firstNumber + secondNumber;
+            dynamic n1 = firstNumber, n2 = secondNumber;
+            var ans = n1 + n2;
             Console.WriteLine("Sum of the result is {0}", ans);
         }
-        public static void Sub(int firstNumber, int secondNumber)
+        public static void Sub<T, U>(T firstNumber, U secondNumber)
         {
-            int ans = firstNumber - secondNumber;
+            dynamic n1 = firstNumber, n2 = secondNumber;
+            var ans = n1 - n2;
             Console.WriteLine("Sub of the result is {0}", ans);
         }
     }
